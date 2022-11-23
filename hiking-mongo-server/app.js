1ce4d1793088144b31e9d697e709b86dd4bc36a7
Jack Gimbert <58886814+jaggimbert@users.noreply.github.com>
2022-11-23 15:18:01 -0500

    cors shit

--- hiking-mongo-server/app.js
+++ hiking-mongo-server/app.js
@@ -6,11 +6,7 @@ const { response } = require("express");
 const mongo = require("./mongo.js");
 const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "http://localhost:5000",
-  })
-);
+app.use(cors());
 app.use(
   basicAuth({
     users: {
