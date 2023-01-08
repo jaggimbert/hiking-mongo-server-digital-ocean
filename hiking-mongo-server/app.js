73e3779d7a2cc7d28e09d9bd2fdc0a1cbb1b175b
Jack Gimbert <58886814+jaggimbert@users.noreply.github.com>
2023-01-08 09:56:22 -0500

    installed mongoose and did initial setup

--- hiking-mongo-server/app.js
+++ hiking-mongo-server/app.js
@@ -5,79 +5,119 @@ const bodyParser = require("body-parser");
 const { response } = require("express");
 const mongo = require("./mongo.js");
 const cors = require("cors");
+const mongoClient = require("./mongoClient.js");
+const client = mongoClient.getMongoClient();
+const mongoose = require("mongoose");
 
-app.use(cors());
-app.use(
-  basicAuth({
-    users: {
-      jacques: "MyManJackyBoy",
-    },
-  })
+mongoose.connect(
+  "mongodb+srv://jaggimbert:jiggaboo23@cluster0.pshyhnp.mongodb.net/mydb"
+);
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    createddate: String,
+    title: String,
+    description: String,
+    lat: String,
+    lng: String,
+    video_url: String,
+  },
+  { collection: "jackData" }
 );
 
-/**
-  START
-  SHELTER APIS
-*/
-
-// Retrieve shelter data
-app.get("/shelters", async (req, res) => {
-  const data = await mongo.getMongoData("shelterData");
-  res.send(data);
-});
-
-/**
-  END
-  SHELTER APIS
-*/
-
-/**
-  START
-  JACK POSTS APIS
-*/
+const postData = mongoose.model("PostData", postSchema);
 
 // Retrieve jack data
-app.get("/jackData", async (req, res) => {
-  const data = await mongo.getMongoData("jackData");
-  res.send(data);
+app.get("/jackData", async (req, res, next) => {
+  postData.find((err, data) => {
+    console.log(data);
+    res.send(data);
+  });
 });
 
-// Insert post
-app.post("/jackData", bodyParser.json(), async (req, res) => {
-  try {
-    const response = await mongo.insertMongoData("jackData", req.body);
-    res.status(200).send(`Post successfully inserted`);
-  } catch (error) {
-    res.status(500).send(`Error while attempting to insert post: ${error}`);
-  }
-});
+// app.use(cors());
+// app.use(
+//   basicAuth({
+//     users: {
+//       jacques: "MyManJackyBoy",
+//     },
+//   })
+// );
+// /**
+//   START UTILITY APIS
+// */
+// app.post("/shelters", async (req, res) => {
+//   const data = await mongo.getMongoData("shelterData", client);
+//   res.send(data);
+// });
+// /**
+//   END UTILITY APIS
+// */
 
-// Edit post
-app.put("/jackData", bodyParser.json(), async (req, res) => {
-  try {
-    const response = await mongo.editMongoData("jackData", req.body);
-    res.status(200).send(`Post successfully edited`);
-  } catch (error) {
-    res.status(500).send(`Error while attempting to edit post: ${error}`);
-  }
-});
+// /**
+//   START
+//   SHELTER APIS
+// */
 
-// Delete post
-/**
-  @param req pass objectId like so: { "_id" : ObjectId("636aef3cb7b07704bc64745c") }
-*/
-app.delete("/jackData", bodyParser.json(), async (req, res) => {
-  try {
-    const response = await mongo.deleteMongoData("jackData", req.body);
-    res.status(200).send(`Post successfully deleted`);
-  } catch (error) {
-    res.status(500).send(`Error while attempting to delete post: ${error}`);
-  }
-});
+// // Retrieve shelter data
+// app.get("/shelters", async (req, res) => {
+//   const data = await mongo.getMongoData("shelterData", client);
+//   res.send(data);
+// });
 
-/**
-  END
-  JACK POSTS APIS
-*/
+// /**
+//   END
+//   SHELTER APIS
+// */
+
+// /**
+//   START
+//   JACK POSTS APIS
+// */
+
+// // Retrieve jack data
+// app.get("/jackData", async (req, res) => {
+//   const data = await mongo.getMongoData("jackData", client);
+//   res.send(data);
+// });
+
+// // Insert post
+// app.post("/jackData", bodyParser.json(), async (req, res) => {
+//   try {
+//     const response = await mongo.insertMongoData("jackData", client, req.body);
+//     res.status(200).send(`Post successfully inserted`);
+//   } catch (error) {
+//     res.status(500).send(`Error while attempting to insert post: ${error}`);
+//   }
+// });
+
+// // Edit post
+// app.put("/jackData", bodyParser.json(), async (req, res) => {
+//   try {
+//     const response = await mongo.editMongoData("jackData", client, req.body);
+//     res.status(200).send(`Post successfully edited`);
+//   } catch (error) {
+//     res.status(500).send(`Error while attempting to edit post: ${error}`);
+//   }
+// });
+
+// // Delete post
+// /**
+//   @param req pass objectId like so: { "_id" : ObjectId("636aef3cb7b07704bc64745c") }
+// */
+// app.delete("/jackData", bodyParser.json(), async (req, res) => {
+//   try {
+//     const response = await mongo.deleteMongoData("jackData", client, req.body);
+//     res.status(200).send(`Post successfully deleted`);
+//   } catch (error) {
+//     res.status(500).send(`Error while attempting to delete post: ${error}`);
+//   }
+// });
+
+// /**
+//   END
+//   JACK POSTS APIS
+// */
 
 app.listen(5000);
--- hiking-mongo-server/mongo.js
+++ hiking-mongo-server/mongo.js
@@ -1,27 +1,25 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const bson = require("bson");
 
 //Connect to mongo and return all data for specified collection
-const getMongoData = (collectionName) => {
-  const client = getMongoClient();
-  const collection = client.db("mydb").collection(collectionName);
-  const cursor = collection.find();
-  return cursor.toArray();
+const getMongoData = async (collectionName, client) => {
+  // const client = await getMongoClient();
+  const collection = await client.db("mydb").collection(collectionName);
+  const data = await collection.find().toArray();
+  return data;
 };
 
 //Connect to mongo and insert data
-const insertMongoData = async (collectionName, req) => {
-  const client = getMongoClient();
-  const collection = client.db("mydb").collection(collectionName);
+const insertMongoData = async (collectionName, client, req) => {
+  // const client = await getMongoClient();
+  const collection = await client.db("mydb").collection(collectionName);
   const result = await collection.insertOne(req);
   return result;
 };
 
 //Connect to mongo and edit data
-const editMongoData = async (collectionName, req) => {
-  console.log("request: ", req);
-  const client = getMongoClient();
-  const collection = client.db("mydb").collection(collectionName);
+const editMongoData = async (collectionName, client, req) => {
+  // const client = await getMongoClient();
+  const collection = await client.db("mydb").collection(collectionName);
   const result = await collection
     .updateOne(
       { _id: bson.ObjectID(req.Id) },
@@ -30,28 +28,15 @@ const editMongoData = async (collectionName, req) => {
       }
     )
     .catch((err) => console.log("MONGO ERROR: ", err));
-  console.log("MONGO UPDATE RESULT: ", result);
   return result;
 };
 
 //Connect to mongo and delete data
-const deleteMongoData = async (collectionName, req) => {
-  const client = getMongoClient();
+const deleteMongoData = async (collectionName, client, req) => {
+  // const client = getMongoClient();
   const collection = client.db("mydb").collection(collectionName);
   const result = await collection.deleteOne({ _id: bson.ObjectID(req.Id) });
   return result;
-};
-
-//Initialize Mongo Client
-const getMongoClient = () => {
-  const uri =
-    "mongodb+srv://jaggimbert:jiggaboo23@cluster0.pshyhnp.mongodb.net/?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
-  });
-  return client;
 };
 
 exports.getMongoData = getMongoData;
