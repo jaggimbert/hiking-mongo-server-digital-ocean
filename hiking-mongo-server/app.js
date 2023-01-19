d6c1663ca1ed45cb44cd2a7dac8372e6c203fa64
Jack Gimbert <58886814+jaggimbert@users.noreply.github.com>
2023-01-19 14:56:05 -0500

    test

--- hiking-mongo-server/app.js
+++ hiking-mongo-server/app.js
@@ -96,7 +96,7 @@ app.post("/jackData", isAuth, bodyParser.json(), (req, res, next) => {
 
 app.put("/jackData", isAuth, bodyParser.json(), async (req, res) => {
   console.log("post: ", req.body);
-  console.log("id: ", req.body.post["id"]);
+  console.log("id: ", req.body.post["_id"]);
   let id = req.body.post["_id"];
 
   postData.findById(id, (err, post) => {
